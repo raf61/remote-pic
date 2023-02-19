0b4e96bbb0def328de9864cc84c18787c67d2d12
rafael <rafael@kali.kali>
2023-02-19 15:28:19 -0300

    adjusts

---
+++ error.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-full h-[90vh] flex justify-center items-center text-white">
+        <div class="max-w-sm mx-auto text-center">
+            <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+            <p class="text-6xl">Ooops.</p>
+            <p class="mt-7">{{ error.message }}</p>
+            <button class="btn my-7 underline" @click="handleClearError">Go Home ...</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+const {error} = defineProps({
+    error:Object
+})
+
+const handleClearError = () => clearError({
+    redirect:'/'
+})
+
+useHead({
+    title:error.message,
+    bodyAttrs:{
+        class:'bg-custom-gray'
+    }
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
                     </div>
                     <div class="mt-5 text-white flex justify-center" v-else>
                         <div class="w-full flex justify-center">
-                            <button ref="uploadButton" @click="handleUploadButton" class="flex items-center justify-center w-[13rem] bg-blue-600 transition duration-500 rounded-full cursor-pointer text-center px-2 py-3 md:py-2 border-2s border-blue-600 hover:bg-white hover:text-blue-600">
+                            <button ref="uploadButton" @click="handleUploadButton" class="flex items-center justify-center w-[13rem] bg-blue-600 transition duration-500 rounded-full cursor-pointer text-center px-2 py-3 md:py-2 border-2s border-blue-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                 </svg>
@@ -50,7 +50,11 @@
 </template>
 
 <script setup>
-
+    useHead({
+        bodyAttrs:{
+            class:'bg-custom-gray'
+        }
+    })
     const chosenFile = ref(null)
     const uploadButton = ref()
     const uploadInput = ref()
@@ -105,9 +109,3 @@
     }
 
 </script>
-
-<style>
-body{
-    background-color:#333538;
-}
-</style>
